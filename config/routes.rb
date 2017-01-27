eb969cd755eea1cb9c05ad399a1ff13367f22ef7
zhoujin <jin100chow@hotmail.com>
2017-01-27 14:20:05 +0800

    add downvote and sort_by votes count

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
    root 'topics#index'
-   resources :topics
+   resources :topics do
+     member do
+       post 'upvote'
+     end
+     member do
+       post 'downvote'
+     end
+   end
 
 end
